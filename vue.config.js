c3a08f53d78f3dca11aca5f2e72f0d8b0168e287
yangqiangxidian@163.com <yangqiangxidian@163.com>
2020-07-05 23:22:27 +0800

    配置压缩包和自动部署

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
-//
-// const webpack = require('webpack')
-//
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const version = "1.0";
+
 module.exports = {
   devServer: {
     proxy: {
@@ -10,5 +10,16 @@ module.exports = {
         pathRewrite: {'^/api' : ''}
       }
     }
+  },
+  configureWebpack:{
+    plugins: [
+      new FileManagerPlugin({  //初始化 filemanager-webpack-plugin 插件实例
+        onEnd: {
+          archive: [ //然后我们选择dist文件夹将之打包成dist.zip并放在根目录
+            {source: './dist', destination: `./deploy/dist-${version}.zip`},
+          ]
+        }
+      })
+    ]
   }
 }
